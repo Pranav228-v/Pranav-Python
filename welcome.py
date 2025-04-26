b1fb50b77e2e028b61f95e19058aa026f2c7b24a
Pranav228-v <niramalavino@gmail.com>
2025-04-26 20:33:04 +0530

    Welcome

---
+++ welcome.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello python \n")
+
+#Pass multiple arguements for printing
+print("hello", 5)
+
+#End arguement of print statement
+print("welcome to", end="+")
