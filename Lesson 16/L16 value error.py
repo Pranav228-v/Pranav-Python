278e4ac1499aa3e15ad279081ccb82aaf8660870
Pranav228-v <niramalavino@gmail.com>
2025-06-14 20:26:02 +0530

    Value error

---
+++ Lesson 16/L16 value error.py
@@ -0,0 +1,12 @@
+try :
+    num = int(input("Enter your number: ")) 
+    print("the number entered is: ",num)
+
+except NameError as e: #using NameError
+    print("Exception: ",e)
+
+except ValueError as ve: #using valueError
+    print("Exception: ",ve)
+
+print("I am outside the try-except block") #always executed and displayed the message
+print()
