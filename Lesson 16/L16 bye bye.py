4a15f1850da924880f7348b9e6be4b2631d5cbba
Pranav228-v <niramalavino@gmail.com>
2025-06-14 20:55:08 +0530

    Bye Bye

---
+++ Lesson 16/L16 bye bye.py
@@ -0,0 +1,14 @@
+valid = False
+
+while not valid: #outer loop
+    try:
+        n = int(input("Enter a number: "))
+
+        #enter an even number
+        while n%2 == 0: #inner loop
+            print("bye")
+            valid = True
+            break
+    
+    except ValueError:
+        print("Invalid")
