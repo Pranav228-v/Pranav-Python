e68fe02ed19bdd18509198fbf219b0fd80519aad
Pranav228-v <niramalavino@gmail.com>
2025-07-11 20:34:15 +0530

    Zip it

---
+++ Lesson 23/L23 zip it.py
@@ -0,0 +1,23 @@
+# zip elements of 2 sets
+s1 = {2, 3, 1}
+s2 = {'b', 'a', 'c'}
+s3 = list(zip(s1, s2))
+print(s3)
+print()
+
+#zip elements of 2 lists
+#print elements one by one, but elements of 2nd list will be in reverse order
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+
+#zip into dictionary
+stocks = ['reliance', 'ifosoys', 'tcs']
+prices = [2175, 1127, 2750]
+
+new_dict = {stocks: prices for stocks,
+            prices in zip(stocks, prices)}
+
+print('\n{}'.format(new_dict))
