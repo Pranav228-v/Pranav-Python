a690b4ed36a938e741b299d9a71efa88ccfb0acd
Pranav228-v <niramalavino@gmail.com>
2025-08-08 20:47:58 +0530

    Add imageto the screen

---
+++ Lesson 31/L31 add image to the screen.py
@@ -0,0 +1,42 @@
+import pygame
+
+#initalize required modules
+pygame.init()
+white = (255, 255, 255)
+
+#clock
+clock = pygame.time.Clock()
+
+#creating the display surface object
+#of specific dimension..e(X, Y).
+display_surface = pygame.display.set_mode((500, 500))
+
+#set the pygame window name
+pygame.display.set_caption('Image')
+
+#creating a surface object, image is drawn on it.
+image = pygame.image.load('Lesson 31/cat.jpg')
+
+#set the size for the image
+DEFAULT_IMAGE_SIZE = (200, 200)
+
+#scale the image to your needed size
+image = pygame.transform.scale(image, DEFAULT_IMAGE_SIZE)
+
+#set a default position
+DEFAULT_IMAGE_POSITION = (150,150)
+
+#infinite loop
+while True:
+    display_surface.fill(white)
+    display_surface.blit(image, DEFAULT_IMAGE_POSITION)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            #quit the program
+            quit()
+
+    #part of event loop
+    pygame.display.flip()
+    clock.tick(30)
