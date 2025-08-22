ff94a4440c01cf792dd0870ab89ed63fd23351ea
Pranav228-v <niramalavino@gmail.com>
2025-08-22 20:54:21 +0530

    Space Invader pt1

---
+++ Lesson 35-36/L35-L36 space Invader.py
@@ -0,0 +1,72 @@
+import math
+import random
+
+import pygame
+from pygame import mixer
+
+#initialize the pygame
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((800, 500))
+
+#background
+background = pygame.image.load('Lesson 35-36/background.png')
+
+#sound
+mixer.music.load("Lesson 35-36/background.wav")
+mixer.music.play(-1)
+
+#caption and icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load('Lesson 35-36/ufo.png')
+pygame.display.set_icon(icon)
+
+#player
+playerImg = pygame.image.load('Lesson 35-36/player.png')
+playerX = 370
+playerY = 380
+playerX_change = 0
+
+#enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 3
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('Lesson 35-36/enemy.png'))
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(4) #make the alien moving right automatically
+    enemyY_change.append(40) #make the alien moving down automatically
+
+#bullet
+#ready - you can't see the bullet on the screen
+#fire - the bullet is currently moving
+bulletImg = pygame.image.load('Lesson 35-36/bullet.png')
+bulletX = 0
+bulletY = 380
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
+#score
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+testY = 10
+
+#game over
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
